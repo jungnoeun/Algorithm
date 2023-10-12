51d23fa7fbdeddc8f29904baa5362a7c2a9704ae
lovee9523 <noeun1102@gmail,com>
2023-10-13 02:25:09 +0900

    스택 수열 더 보기 쉽게

--- BAEKJOON/STackAndQueue/B1874.java
+++ BAEKJOON/STackAndQueue/B1874.java
@@ -9,40 +9,29 @@ public class B1874 {
         // n (몇번인지) 입력받음
         Scanner sc = new Scanner(System.in);
         int time = sc.nextInt();
-        int[] arr = new int[time];
-
-        for(int i=1; i<=time; i++) {
-            arr[i-1] = sc.nextInt();
-        }
-        int num = 1;
         int input;
         StringBuilder sb = new StringBuilder();
-        boolean result = true;
+
+        int start = 0;
+
 
         // 하나씩 입력받으면서 pop/push 결정
         for(int i=0; i<time; i++) {
-            input = arr[i];
-            if(input >= num) {
-                while(input >= num) {
-                    stack.push(num++);
+            input = sc.nextInt();
+
+            if(input > start) {
+                for(int j=start+1; j<=input; j++) {
+                    stack.push(j);
                     sb.append("+\n");
                 }
-                stack.pop(); // 같게 만들어주고 출력을 위해 pop
-                sb.append("-\n");
-            } else {
-                int pop = stack.pop(); // pop이 5이고 출력해야 하는 값이 3일 경우, 3이 아닌 5를 pop해야 하므로 3이 아닌 값을 출력하게 된다. 그래서 NO.
-                if(pop > input) {
-                    System.out.println("NO");
-                    result = false;
-                    break;
-                } else { // input과 pop 이 같은 수이면
-                    sb.append("-\n");
-                }
+                start = input;
+            } else if(stack.peek() != input) {
+                System.out.println("NO");
+                return;
             }
+            stack.pop();
+            sb.append("-\n");
         }
-
-        if(result) System.out.println(sb.toString());
-
+        System.out.println(sb.toString());
     }
-
 }
