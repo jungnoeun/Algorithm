e9ce126be3725589cd1f4a81025b945c1bebe2da
lovee9523 <noeun1102@gmail.com>
2023-10-31 15:00:10 +0900

    그리디 이코테 큰수의 법칙

---
+++ BAEKJOON/Greedy/TlawOfLargeNumbers.java
@@ -0,0 +1,46 @@
+package BAEKJOON.Greedy;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class TlawOfLargeNumbers {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int K = sc.nextInt();
+        int result = 0;
+
+        // 리스트 값 입력받기
+        Integer[] list = new Integer[N];
+        for(int i=0; i<N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        // 큰 수를 만들기 위해 가장 큰 값과 두 번째로 큰 값을 골라냄
+        Arrays.sort(list, Collections.reverseOrder());
+        int max = list[0];
+        int sec = 0;
+        for(int i=1; i<N; i++) {
+            if(list[0] != list[i]) {
+                sec = list[i];
+                break;
+            }
+        }
+
+        // 큰 수 만들기
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<K; j++) {
+                result += max;
+                i++;
+            }
+            result += sec;
+            i++;
+        }
+
+        System.out.println(result);
+
+    }
+
+}
