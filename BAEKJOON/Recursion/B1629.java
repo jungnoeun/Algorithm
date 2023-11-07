5c4e8c20c3411d9eef74443236f9d895efa1343d
lovee9523 <noeun1102@gmail.com>
2023-11-07 15:40:35 +0900

    재귀 - 지수법칙, 분배법칙 사용

--- BAEKJOON/Recursion/B1629.java
+++ BAEKJOON/Recursion/B1629.java
@@ -10,26 +10,26 @@ public class B1629 {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        int C = sc.nextInt();
+        long C = sc.nextLong();
 
         sc.close();
-        dfs(A,B,C,0);
+        dfs(A,B,C);
         System.out.println(result);
 
     }
 
-    static void dfs(int A, int B, int C, int cnt) {
-        if (B == cnt) {
-            return;
+    static long dfs(int A, int B, long C) {
+        if (B == 0) {
+            return 1;
         }
 
-        result *= A;
-        if (result >= C) {
-            result %= C;
-        }
-        cnt++;
+        long n = dfs(A,B/2, C); // {(A^n * A^n) * (A^n * A^n)} % C ==  (A^n * A^n %C) * (A^n * A^n %C)
 
-        dfs(A, B, C, cnt);
+        if(B%2 == 0) { // 짝수번 곱하면
+            return n * n % C; // a^4 = a^2 * a^2
+        } else { // 홀수번 곱하면
+            return (n*n%C) * (A % C); // a^5 = a^3 * a^2
+        }
 
     }
 }
