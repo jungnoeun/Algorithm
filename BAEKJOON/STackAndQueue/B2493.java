653e994757983c341206ad76169c7bbac11dd313
lovee9523 <noeun1102@gmail.com>
2023-11-07 15:10:45 +0900

    스택 - 배열을 없애고 스택에 이차원 배열 배열을 저장하여 사용

--- BAEKJOON/STackAndQueue/B2493.java
+++ BAEKJOON/STackAndQueue/B2493.java
@@ -2,7 +2,6 @@ package BAEKJOON.STackAndQueue;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -12,41 +11,26 @@ public class B2493 {
         int N = Integer.parseInt(br.readLine());
         int[] top = new int[N];
 
-        Stack<Integer> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
+        Stack<int[]> stack = new Stack<>(); // (탑 높이, 탑 순서)
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) { // 탑의 높이들을 입력받음
-            top[i] = Integer.parseInt(st.nextToken());
-        }
 
-        sb.append("0 ");
-
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j <= i; j++) {
-                stack.push(top[j]);
-            }
-            int now = stack.peek();
-            int index = i+1;
-            boolean isBig = false;
-            while(!stack.isEmpty()) {
-                int pop = stack.pop();
-                if(pop > now) {
-                    isBig = true;
+        for (int i = 0; i < N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            while(!stack.isEmpty()) { // 스택이 비어있지 않으면
+                if(stack.peek()[0] < now) { // 맨앞의 탑이 현재 탑보다 작으면 pop
+                    stack.pop();
+                } else { // 맨 앞의 탑이 현재 탑보다 크면 해당 탐의 순서를 출력
+                    System.out.print(stack.peek()[1] + " ");
                     break;
                 }
-                index--;
             }
-            if(isBig) {
-                sb.append(index).append(" ");
-            } else {
-                sb.append("0 ");
+            if(stack.isEmpty()) { // 스택이 비어있으면 전부 탐색한 것이므로 0을 출력
+                System.out.print("0 ");
             }
-            stack.clear();
+
+            stack.push(new int[]{now, i + 1}); // 탑의 높이와 순서를 하나씩 넣음 6부터...
 
         }
 
-        System.out.println(sb.toString());
-        br.close();
     }
 }
